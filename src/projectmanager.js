20e6effbbeccb92dc4802837b89dda77f82ad812
Yudhi <baskyudh@gmail.com>
2025-03-17 13:36:14 +0700

    Add new methods and delegations - Add save and load from local storage functionality - Add duplicate project prevention to addNewProject - Add project details (name, color) delegations - Add task details delegations (title, descriptions, etc.) - Add initial project if local storage is empty;

--- src/projectmanager.js
+++ src/projectmanager.js
@@ -1,10 +1,45 @@
 import Project from "./project.js";
+import { saveToStorage, loadFromStorage } from "./storage.js";
+import { addDays } from "date-fns";
 
 function ProjectManager() {
-  const projects = [];
+  const storageKey = "projects";
+
+  const projects = loadFromStorage(storageKey).map((p) => Project(p.name, p.color, p.tasks));
+
+  const saveProjects = () => saveToStorage(storageKey, projects);
+  
+
   const addNewProject = (name, color) => {
+    for (let project of projects) {
+      if (project.getName() === name) {
+        console.log("project name exists!");
+        return;
+      }
+    }
     const newProject = Project(name, color);
     projects.push(newProject);
+    saveProjects();
+  };
+
+  const renameProject = (name, newName) => {
+    const project = getProject(name);
+    if (!project) {
+      console.log("No such project!");
+      return;
+    }
+    project.setName(newName);
+    saveProjects();
+  };
+
+  const recolorProject = (name, newColor) => {
+    const project = getProject(name);
+    if (!project) {
+      console.log("No such project!");
+      return;
+    }
+    project.setColor(newColor);
+    saveProjects();
   };
 
   const getAllProjects = () => projects;
@@ -24,18 +59,74 @@ function ProjectManager() {
       console.log("No such project!");
       return;
     }
-    return projects.splice(index, 1);
+    projects.splice(index, 1);
+    saveProjects();
   };
 
-  const allProjectString = () => {
-    return projects.map((project) => project.toString()).join("\n");
+  const allProjectsStr = () => {
+    return projects.map((project) => project.toStr()).join("\n");
+  };
+
+  const newTask = (projectName, ...taskDetails) => {
+    const project = getProject(projectName);
+    project.addTask(...taskDetails);
+    saveProjects();
+  };
+
+  const removeTask = (projectName, taskTitle) => {
+    const project = getProject(projectName);
+    project.removeTask(taskTitle);
+    saveProjects();
+  };
+
+  const changeTaskTitle = (projectName, taskTitle, newTitle) => {
+    const project = getProject(projectName);
+    project.setTitle(taskTitle, newTitle);
+    saveProjects();
+  };
+
+  const changeTaskDescription = (projectName, taskTitle, newDesc) => {
+    const project = getProject(projectName);
+    project.setDescription(taskTitle, newDesc);
+  };
+
+  const changeTaskDueDate = (projectName, taskTitle, newDate) => {
+    const project = getProject(projectName);
+    project.setDueDate(taskTitle, newDate);
+  };
+
+  const changeTaskPriority = (projectName, taskTitle, newPriority) => {
+    const project = getProject(projectName);
+    project.setPriority(taskTitle, newPriority);
+    saveProjects();
+  };
+
+  if (projects.length === 0) {
+    addNewProject("Home", "black");
+    const dueDate = addDays(new Date(), 7);
+    newTask("Home", {
+      title: "Welcome to Get it Done!",
+      description: "We will help you manage your projects and their tasks!",
+      dueDate: dueDate,
+      priority: "High",
+      status: "pending",
+    });
   }
   return {
+    saveProjects,
     addNewProject,
+    renameProject,
+    recolorProject,
     getAllProjects,
     getProject,
     deleteProject,
-    allProjectString,
+    newTask,
+    removeTask,
+    changeTaskTitle,
+    changeTaskDescription,
+    changeTaskDueDate,
+    changeTaskPriority,
+    allProjectsStr,
   };
 }
 
