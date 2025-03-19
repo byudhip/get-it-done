646dd52e9f828a1c0e81da5b7785c11a953eecce
Yudhi <baskyudh@gmail.com>
2025-03-19 14:21:20 +0700

    Add event listener to open newTaskModal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import "./styles.css";
 import ProjectManager from "./projectmanager.js";
+import * as utils from "./utils.js";
 import UI from "./ui.js";
 
 const pm = ProjectManager();
+
+const newTask = document.querySelector(".add-new-task");
+const taskModal = utils.newTaskModal();
+
+newTask.addEventListener("click", () => taskModal.showModal());
