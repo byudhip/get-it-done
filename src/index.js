2d197a3476c72b41910912aa8186414a97ceb46a
Yudhi <baskyudh@gmail.com>
2025-03-20 10:28:06 +0700

    Add event listeners - Click new task button will open modal with empty fields - Click task div will open modal with pre-populated fields according to clicked task details - Click "x" button on task div will open remove task confirmation modal

--- src/index.js
+++ src/index.js
@@ -5,7 +5,30 @@ import UI from "./ui.js";
 
 const pm = ProjectManager();
 
-const newTask = document.querySelector(".add-new-task");
+const newTaskBtn = document.querySelector(".add-new-task");
+// const newProjectBtn = document.querySelector(".add-new-project");
 const taskModal = utils.newTaskModal();
+const taskForm = document.querySelector("#task-form");
+// const projectModal = utils.newProjectModal();
 
-newTask.addEventListener("click", () => taskModal.showModal());
+const tasksDiv = document.querySelectorAll(".task-main");
+
+newTaskBtn.addEventListener("click", () => {
+  taskModal.showModal();
+});
+
+tasksDiv.forEach((task) => {
+  task.addEventListener("click", (e) => {
+    const taskDiv = e.target.closest(".task-main");
+    const taskDetails = utils.captureDetails(taskDiv);
+    if (e.target.matches(".remove-task-button")) {
+      utils.confirmRemoveTaskModal(taskDetails).showModal();
+    } else { 
+      utils.openEditTaskModal(taskDetails);
+    }
+  });
+});
+
+// newProjectBtn.addEventListener("click", () => {
+//   projectModal.showModal();
+// })
