5f57994e7ac8cfe68b87f9abddef8b032bc8f032
Yudhi <baskyudh@gmail.com>
2025-03-21 13:01:35 +0700

    Remove reapplyListeners method call - Called internally inside renderTasks method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import "./styles.css";
 import ProjectManager from "./projectmanager.js";
 import * as utils from "./utils.js";
-import UI from "./ui.js";
+import {UI} from "./ui.js";
 
 const pm = ProjectManager();
 
 utils.newTaskModal();
 utils.newProjectModal();
-utils.reapplyListeners();
 
